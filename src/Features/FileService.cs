8cfeec2eb89a33fb28218fa4dd999325b65d7eb5
Sahil Syal <sahil.syal@snicsolutions.com>
2023-11-14 18:13:56 +0530

    moved code to services and cleaned up
    moved code to services and cleaned up

---
+++ src/Features/FileService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Windows;
+
+namespace GetNovels.Features
+{
+    public static class FileService
+    {
+        public static void WriteToFile(string filePath, HttpResponseMessage response)
+        {
+            using HttpContent content = response.Content;
+            string result = content.ReadAsStringAsync().GetAwaiter().GetResult();
+            result = HttpStringService.RemoveJunk(result);
+            File.AppendAllText(filePath, result);
+        }
+
+        public static string TryGetPath(string folderPath)
+        {
+            string path;
+            try
+            {
+                path = Path.GetFullPath(folderPath);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Invalid folder path {folderPath}. Exception: {ex.Message}");
+                return "";
+            }
+
+            return path;
+        }
+
+        public static bool TryCreateDirectory(string folderPath)
+        {
+            try
+            {
+                Directory.CreateDirectory(folderPath);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"{ex.Message} Failed at creating Directory.");
+                return false;
+            }
+        }
+    }
+}
---
+++ src/Features/LinkService.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+
+namespace GetNovels.Features
+{
+    public static class LinkService
+    {
+        public static List<string> GetLinks(string data, string baseUrl)
+        {
+            var links = new List<string>();
+            var startIndex = data.IndexOf("\"/");
+            int count = 1;
+            while (startIndex != -1)
+            {
+                var secondIndex = data.IndexOf(".html\"", startIndex);
+                var link = $"{baseUrl}{data.Substring(startIndex + 1, secondIndex - startIndex + 4)}";
+                if (!link.Contains("html"))
+                {
+                    MessageBox.Show($"Issue with link at {link} {count}");
+                    return new List<string>();
+                }
+
+                links.Add(link);
+                count++;
+                startIndex = data.IndexOf("\"/", secondIndex + 1);
+            }
+
+            return links;
+        }
+    }
+}
